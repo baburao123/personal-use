5b927a93d5f5d5615c252a2acf64a86544203065
baburao123 <75316967+baburao123@users.noreply.github.com>
2020-12-06 17:35:30 +0530

    Update index-acton.php

--- index-acton.php
+++ index-acton.php
@@ -12,26 +12,29 @@ if(isset($_POST['url'])){
             break;
         }
     }
+$query = "insert into heroku_da78f8e94b7c5eb.videostore (name,url) values ('".$r."','".$url."')";
+    if($con->query($query)){
+        echo "hello";
+    }
+//     if(file_put_contents("videos/".$r.".mp4",file_get_contents($url))) { 
 
-    if(file_put_contents("videos/".$r.".mp4",file_get_contents($url))) { 
-
-        $query = "insert into heroku_da78f8e94b7c5eb.videostore (name,url) values ('".$r."','".$url."')";
-        if($con->query($query)){
-?>
-<div class="card">
-    <div class="card-img-top">
-        <video class="video-js" controls preload="auto" data-setup="{}" style="width:400px;height:225px">
-            <source src="videos/<?=$r.".mp4";?>" type="video/mp4" />
-            <source src="videos/<?=$r.".webm";?>" type="video/webm" />
-            <source src="videos/<?=$r.".avi";?>" type="video/avi" />
-            <source src="videos/<?=$r.".mkv";?>" type="video/mkv" />
-        </video>
-    </div>                    
-</div>
-<script src="https://vjs.zencdn.net/7.10.2/video.js"></script>
-<?php
-                               }else echo 1;
-    }else echo 1;
+//         $query = "insert into heroku_da78f8e94b7c5eb.videostore (name,url) values ('".$r."','".$url."')";
+//         if($con->query($query)){
+// ?>
+// <div class="card">
+//     <div class="card-img-top">
+//         <video class="video-js" controls preload="auto" data-setup="{}" style="width:400px;height:225px">
+//             <source src="videos/<?=$r.".mp4";?>" type="video/mp4" />
+//             <source src="videos/<?=$r.".webm";?>" type="video/webm" />
+//             <source src="videos/<?=$r.".avi";?>" type="video/avi" />
+//             <source src="videos/<?=$r.".mkv";?>" type="video/mkv" />
+//         </video>
+//     </div>                    
+// </div>
+// <script src="https://vjs.zencdn.net/7.10.2/video.js"></script>
+// <?php
+//                                }else echo 1;
+//     }else echo 1;
 }
 
 ?>
