31bdaab866a3f2bc05c9ae015afd77e75e80c396
baburao123 <75316967+baburao123@users.noreply.github.com>
2020-12-06 17:32:34 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,7 @@
 <?php 
-date_default_timezone_set("Asia/Kolkata");
 $dbhost = "us-cdbr-east-02.cleardb.com";
 $dbUsername = "b44d3fa87ef099";
 $dbPassword = "a4715e5d";
 $dbname = "heroku_da78f8e94b7c5eb";
 $con = new mysqli($dbhost, $dbUsername, $dbPassword, $dbname);
-session_start();
 ?>
